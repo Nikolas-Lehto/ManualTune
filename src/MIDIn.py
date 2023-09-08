5125634c355ad6de4c617225989a93cc3bc2413c
LiquidDenton <kristo@kunnamo.net>
2023-09-08 13:59:45 +0300

    Initial commit

---
+++ src/MIDIn.py
@@ -0,0 +1,18 @@
+import mido
+import numpy as np
+
+
+
+# MIDI to frequency conversion function
+def midi_to_frequency(note_number):
+    return 440.0 * (2 ** ((note_number - 69) / 12.0))
+
+def getMIDI(DeviceName):
+
+    # Set up MIDI input
+    midi_in = mido.open_input(DeviceName)
+    for msg in midi_in.iter_pending():
+        if msg.type == 'note_on':
+            frequency = midi_to_frequency(msg.note)
+    return frequency
+print(getMIDI('foo'))
