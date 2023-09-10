072f09a4cc3dd1c112961eb4629a387a86def2f4
Nikolas Lehto <nikolas.lehto1@outlook.com>
2023-09-10 10:53:27 +0300

    Moved MIDIn to a class

--- src/MIDIn.py
+++ src/MIDIn.py
@@ -2,44 +2,64 @@ import mido
 import numpy as np
 import os
 
-# MIDI to frequency conversion function
-def midi_to_frequency(note_number:int):
-    return 440.0 * (2 ** ((note_number - 69) / 12.0))
+class MIDI():
+    def __init__(self, MIDI_framework:str="rtmidi", device:int=0):
+        self.device = device
+        mido.set_backend(MIDI_framework)
 
-def ChooseMIDI(MIDI_framework:str="rtmidi"):
+    def midi_to_frequency(self,note_number:int):
+        """A MIDI to frequency conversion function."""
+        return 440.0 * (2 ** ((note_number - 69) / 12.0))
 
-    mido.set_backend(MIDI_framework)
-    while True:
-        os.system('clear')
-        # Fetch available MIDI devices
-        MIDIlist = mido.get_input_names()
+    def ChooseDevice(self):
+        """A simple CLI to select a midi device."""
+        while True:
+            os.system('clear')
+            # Fetch available MIDI devices
+            DeviceList = mido.get_input_names()
 
-        #Loop through MIDI devices and print them out
-    
-        for i in MIDIlist:
-            print(i + ' ' + MIDIlist[i])
+            #Loop through MIDI devices and print them out
+            for i in range(len(DeviceList)):
+                print(f'{i} {DeviceList[i]}')
 
-        #Ask the user to choose a device
+            #Ask the user to choose a device
+            try:
+                self.device = int(input('Please choose a MIDI device (Press enter to refresh): '))
+                return MIDIlist[self.device]
+            except KeyboardInterrupt:
+                print()
+                print('CTRL+C pressed. Exiting...')
+                return 1
+            except:
+                print('Please input valid integer')
+                continue
 
-        try:
-            bar = int(input('Please choose a MIDI device (Press enter to refresh): '))
-            return MIDIlist[bar]
-        except KeyboardInterrupt:
-            print()
-            print('CTRL+C pressed. Exiting...')
-            return 1
-        except:
-            print('Please input valid integer')
-            continue
+    def ListDevices(self):
+        """Return a list of MIDI devicesin a machine readable (device index, device name) form."""
+        Devices = mido.get_input_names()
+        self.DeviceList =[]
+        for i in range(len(Devices)):
+            DeviceList.append(({i},{Devices[i]}))
+        return self.DeviceList
 
-def getMIDI(DeviceName:str):
+    def SetDevice(self, device:int=0):
+        """Set the MIDI device"""
+        self.device = device
 
-    # Set up MIDI input
-    midi_in = mido.open_input(DeviceName)
-    for msg in midi_in.iter_pending():
-        if msg.type == 'note_on':
-            foo = midi_to_frequency(msg.note)
-            output(True, foo)
-        else:
-            output(False, None)
-    return output
+    def GetInputs(self,Device:int=0):
+        # Set up MIDI input
+        midi_in = mido.open_input(Device)
+        self.msgs = []
+        for msg in midi_in.iter_pending():
+            match msg.type:
+                case 'note_on':
+                    self.msgs.append((True, msg))
+                case 'note_off':
+                    self.msgs.remove((True, msg))                
+                case _:
+                    pass
+            if self.msgs != []:
+                return self.msgs
+            else:
+                return (False, None)
+            
